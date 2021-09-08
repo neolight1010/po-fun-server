e702633012e3d138e6a0651cfbe3f31a53bde51b
Anthony Suárez <neolight1010@gmail.com>
2021-09-08 11:45:03 -0500

    Add link to home page to navbar.

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,5 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = "app"
-urlpatterns = [path("", views.index),
+urlpatterns = [path("", views.index, name="index"),
                path("login/", auth_views.LoginView.as_view(), name="login")]
