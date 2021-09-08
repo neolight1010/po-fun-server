6936181ee3d82cc953a2904e8532c521c8af54f6
Anthony Suárez <neolight1010@gmail.com>
2021-09-08 13:52:40 -0500

    Create register page.

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 app_name = "app"
 urlpatterns = [path("", views.index, name="index"),
-               path("login/", auth_views.LoginView.as_view(), name="login")]
+               path("login/", auth_views.LoginView.as_view(), name="login"),
+               path("register/", views.register, name="register")]
--- app/views.py
+++ app/views.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import authenticate
+from app.forms import RegisterForm
 from django.http.request import HttpRequest
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.contrib.auth import login as login_user
 
 
 def index(request: HttpRequest):
@@ -8,3 +11,23 @@ def index(request: HttpRequest):
 
 def login(request: HttpRequest):
     return render(request, "app/login.html")
+
+
+def register(request: HttpRequest):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get("username")
+            raw_password = form.clean_password2()
+
+            user = authenticate(request, username=username, password=raw_password)
+            login_user(request, user)
+
+            return redirect("app:index")
+    else:
+        form = RegisterForm()
+
+    return render(request, "registration/register.html", {"form": form})
