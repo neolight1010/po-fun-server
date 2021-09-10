09489ba210daaf06723e32e0166d4624ee0d4b95
Anthony Suárez <neolight1010@gmail.com>
2021-09-09 20:18:24 -0500

    Create logout page.

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ from . import views
 app_name = "app"
 urlpatterns = [path("", views.index, name="index"),
                path("login/", auth_views.LoginView.as_view(), name="login"),
+               path("logout/", auth_views.LogoutView.as_view(next_page="/"), name="logout"),
                path("register/", views.register, name="register")]
