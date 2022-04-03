806a475c422455bc649fe632a92a8c0c9887599f
Anthony Suárez <neolight1010@gmail.com>
2022-04-03 16:00:56 -0500

    Refactor sample.validators.

--- sample/validators.py
+++ sample/validators.py
@@ -1,20 +1,41 @@
+from typing import Optional, cast
 from django.db.models.fields.files import FieldFile
 from django.core.exceptions import ValidationError
 from mutagen import File as MutagenFile
 from mutagen import FileType as MutagenFileType
+from mutagen.flac import StreamInfo
+from mutagen import MutagenError
+
+
+PERFECT_PACK_LENGTH_SECONDS = 4 * 60
+"""PO-33 data transfer audios are 4min 10s long."""
+ALLOWED_ERROR_SECONDS = 10
+""" 10 seconds of error are allowed for upload."""
 
 
 def validate_pack_sample_length(sample: FieldFile):
-    # PO-33 data transfer audios are 4min 10s long. Lengths from 4min to 4min
-    # 20s are allowed for upload.
+    audio_file = _load_audio(sample)
 
-    audio: MutagenFileType = MutagenFile(sample.file)  # type: ignore
+    audio_length = cast(StreamInfo, audio_file.info).length
+    _validate_audio_length(audio_length)
 
-    if not audio:
-        raise ValidationError("Audio file not found by Mutagen.")
 
-    audio_length = audio.info.length # type: ignore
+def _load_audio(sample: FieldFile) -> MutagenFileType:
+    try:
+        audio_file: Optional[MutagenFileType] = MutagenFile(sample.file)
+    except MutagenError:
+        raise ValidationError("Error loading audio file.")
 
-    if audio_length < (4 * 60) or audio_length > (4 * 60 + 20):
-        raise ValidationError("Pack sample files (PO-33 data transfer audios) must be\
-        from 4min to 4min 20s long.")
+    if audio_file is None:
+        raise ValidationError("File not detected as audio.")
+
+    return audio_file
+
+
+def _validate_audio_length(length: float) -> None:
+    min_allowed = PERFECT_PACK_LENGTH_SECONDS - ALLOWED_ERROR_SECONDS
+    max_allowed = PERFECT_PACK_LENGTH_SECONDS + ALLOWED_ERROR_SECONDS
+
+    if length < min_allowed or length > max_allowed:
+        raise ValidationError("Pack sample files (PO-33 data transfer audios)\
+        must be from 4min to 4min 20s long.")
