39b3b4ee18d816dbd9827d091ce22ad47c55bcee
Anthony Suárez <neolight1010@gmail.com>
2022-08-07 10:50:56 -0500

    Write test for login view

--- app/tests.py
+++ app/tests.py
@@ -8,3 +8,11 @@ class IndexViewTests(SimpleTestCase):
         response = client.get(reverse("app:index"))
         
         self.assertNotEqual(response.status_code, 404)
+
+class LoginViewTests(SimpleTestCase):
+    def test_response_200(self):
+        client = Client()
+
+        response = client.get(reverse("app:login"))
+        
+        self.assertEqual(response.status_code, 200)
