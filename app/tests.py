135ab9fd8a95556faae10882343af5cd77ff4c8c
Anthony Suárez <neolight1010@gmail.com>
2022-08-07 10:52:21 -0500

    Change index view test

--- app/tests.py
+++ app/tests.py
@@ -2,12 +2,12 @@ from django.test import Client, SimpleTestCase
 from django.urls import reverse
 
 class IndexViewTests(SimpleTestCase):
-    def test_not_404(self):
+    def test_response_200(self):
         client = Client()
 
         response = client.get(reverse("app:index"))
         
-        self.assertNotEqual(response.status_code, 404)
+        self.assertEqual(response.status_code, 200)
 
 class LoginViewTests(SimpleTestCase):
     def test_response_200(self):
