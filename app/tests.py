933fcf28227543174c9bc6305870f2e492c9dc7a
Anthony Suárez <neolight1010@gmail.com>
2022-08-07 15:18:05 -0500

    Add simple tests for register view

--- app/tests.py
+++ app/tests.py
@@ -8,3 +8,18 @@ class IndexViewTests(SimpleTestCase):
         response = client.get(reverse("app:index"))
         
         self.assertEqual(response.status_code, 200)
+
+class RegisterViewTests(SimpleTestCase):
+    def test_get_response_200(self):
+        client = Client()
+
+        response = client.get(reverse("app:register"))
+        
+        self.assertEqual(response.status_code, 200)
+
+    def test_post_response_200(self):
+        client = Client()
+
+        response = client.post(reverse("app:register"))
+        
+        self.assertEqual(response.status_code, 200)
