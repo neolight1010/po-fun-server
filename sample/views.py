c04ae97b8736180ae69d2ba681e009b25b0bcb74
Anthony Suárez <neolight1010@gmail.com>
2022-08-13 10:31:06 -0500

    Make sample.views.get_sample_views private

--- sample/views.py
+++ sample/views.py
@@ -9,7 +9,7 @@ from .models import Sample
 from .forms.SampleForm import SampleForm
 
 
-def get_samples_view(sample_type: Sample.SampleType):
+def _get_samples_view(sample_type: Sample.SampleType):
     class View(ListView):
         paginate_by = 6
         queryset = Sample.objects.filter(sample_type=sample_type)
@@ -18,9 +18,9 @@ def get_samples_view(sample_type: Sample.SampleType):
     return View
 
 
-DrumsView = get_samples_view(Sample.SampleType.DRUM)
-MelodicView = get_samples_view(Sample.SampleType.MELODIC)
-PacksView = get_samples_view(Sample.SampleType.PACK)
+DrumsView = _get_samples_view(Sample.SampleType.DRUM)
+MelodicView = _get_samples_view(Sample.SampleType.MELODIC)
+PacksView = _get_samples_view(Sample.SampleType.PACK)
 
 
 @login_required
