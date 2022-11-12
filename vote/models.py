c042d8f4eece58f0eb3bfec1e6dd608211f75826
Anthony Suárez <neolight1010@gmail.com>
2022-11-11 19:35:08 -0500

    Create Vote model

--- vote/models.py
+++ vote/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from sample.models import Sample
 
-# Create your models here.
+from user.models import User
+
+
+class Vote(models.Model):
+    class Direction(models.IntegerChoices):
+        UP = 0
+        DOWN = 1
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    sample = models.ForeignKey(Sample, on_delete=models.CASCADE)
+
+    direction = models.IntegerField(choices=Direction.choices)
