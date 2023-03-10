fce770c57650ac721deb56a04cbf77f5d5cd5c9f
Anthony Suárez <neolight1010@gmail.com>
2023-03-09 20:56:46 -0500

    Add support for voting and downvoting samples
    Closes #11.

--- vote/tests.py
+++ vote/tests.py
@@ -1,37 +1,44 @@
-from typing import Callable
-from typing_extensions import Self
 import django.urls as urls
 
 from django.test import TestCase
 from django.test.client import Client
-
 from sample.models import Sample
+
 from user.models import User
 from vote.models import Vote
 
 
 class VoteViewTests(TestCase):
-    def test_requires_login(self) -> None:
-        client = Client()
+    def setUp(self) -> None:
+        self.client = Client()
 
-        response = client.post(self._vote_view_url(0), {"direction": 0})
+    def test_requires_login(self) -> None:
+        response = self.client.post(self._vote_view_url(0), {"direction": 0})
 
         self.assertEqual(response.status_code, 302)
 
-    def test_validates_schema(self) -> None:
-        client = Client()
+    def test_validates_missing_sample(self) -> None:
+        self.client.force_login(self._simple_user())
 
-        client.force_login(self._simple_user())
-
-        response = client.post(self._vote_view_url(0), {"direction": Vote.Direction.UP})
+        response = self.client.post(self._vote_view_url(
+            0), {"direction": Vote.Direction.UP})
 
         self.assertEqual(response.status_code, 400)
+
+    def test_upvote(self) -> None:
+        user = self._simple_user()
+        self.client.force_login(user)
+
+        sample = Sample.objects.create(
+            author=User.objects.create(username="John"))
+        self.client.post(self._vote_view_url(sample.id), {
+            "direction": Vote.Direction.UP})
+
+        self.assertTrue(Vote.objects.filter(
+            sample=sample, direction=Vote.Direction.UP, user=user).exists())
 
     def _vote_view_url(self, sample_id: int) -> str:
         return urls.reverse("vote:vote", args=[sample_id])
 
     def _simple_user(self) -> User:
-        user = User()
-        user.save()
-
-        return user
+        return User.objects.create()
--- vote/views.py
+++ vote/views.py
@@ -1,19 +1,32 @@
+from typing import cast
 from django.contrib.auth.decorators import login_required
-import pydantic
+import django.forms as forms
 
 from django.http import HttpRequest, HttpResponseBadRequest
 from django.shortcuts import HttpResponse
+from sample.models import Sample
+from user.models import User
 
 from vote.models import Vote
 
 
-class _VoteViewData(pydantic.BaseModel):
-    direction: Vote.Direction | None = pydantic.Field(...)
+class _VoteViewForm(forms.Form):
+    direction = forms.ChoiceField(choices=Vote.Direction.choices)
 
 
 @login_required
 def vote_view(request: HttpRequest, sample_id: int) -> HttpResponse:
-    try:
-        request_data = _VoteViewData(**request.POST)
-    except pydantic.ValidationError:
+    request_data = _VoteViewForm(request.POST)
+
+    if not request_data.is_valid():
+        return HttpResponseBadRequest(request_data.errors.as_json())
+
+    sample = Sample.objects.filter(id=sample_id).first()
+
+    if sample is None:
         return HttpResponseBadRequest()
+
+    Vote.objects.create(sample_id=sample_id,
+                        direction=request_data.cleaned_data['direction'], user=cast(User, request.user))
+
+    return HttpResponse()
