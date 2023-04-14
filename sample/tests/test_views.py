2536f7eb92db7bc087db2e8cc29e8ea23f4fee32
Anthony Suárez <neolight1010@gmail.com>
2023-04-13 20:41:10 -0500

    Refactor sample views tests

--- sample/tests/test_views.py
+++ sample/tests/test_views.py
@@ -1,9 +1,8 @@
 from typing import cast
-from unittest.mock import MagicMock, Mock, patch
 from django.core.files.uploadedfile import SimpleUploadedFile
 from django.forms.fields import Field
 from django.test.testcases import TestCase
-from django.test.client import Client, RequestFactory
+from django.test.client import RequestFactory
 from django.urls.base import reverse
 
 from sample.forms.SampleForm import SampleForm
@@ -18,31 +17,23 @@ _UPLOAD_URL = reverse("sample:upload")
 
 
 class UploadViewsTestCase(TestCase):
-    user: User
-
     def setUp(self) -> None:
         self.user = User.objects.create(username="test")
-
-        return super().setUp()
+        self.client.force_login(self.user)
 
     def test_should_redirect_to_login(self):
-        client = Client()
-        response = client.get(_UPLOAD_URL, follow=True)
+        self.client.logout()
+
+        response = self.client.get(_UPLOAD_URL, follow=True)
 
         self.assertRedirects(response, reverse("app:login") + f"?next={_UPLOAD_URL}")
 
     def test_should_return_200_when_authenticated(self):
-        client = Client()
-        client.force_login(self.user)
-
-        response = client.get(_UPLOAD_URL)
+        response = self.client.get(_UPLOAD_URL)
         self.assertEquals(response.status_code, 200)
 
     def test_get_request(self):
-        client = Client()
-        client.force_login(self.user)
-
-        response = client.get(_UPLOAD_URL)
+        response = self.client.get(_UPLOAD_URL)
 
         form = response.context[0].get("form")
         self.assertIsInstance(form, SampleForm)
@@ -59,34 +50,41 @@ class UploadViewsTestCase(TestCase):
                 self.assertTrue(label.islower(), f"Label '{label}' is not lowercase")
 
     def test_post_request_invalid_data(self):
-        client = Client()
-        client.force_login(self.user)
+        form = SampleForm(data={"invalid": "data"})
 
-        pre_request_samples_quantity = len(Sample.objects.all())
+        response = self.client.post(_UPLOAD_URL, data=form.data)
 
-        response = client.post(_UPLOAD_URL)
         self.assertEquals(response.status_code, 400)
+        self.assertEquals(Sample.objects.count(), 0)
 
-        post_request_samples_quantity = len(Sample.objects.all())
+    def test_post_request_valid_data(self) -> None:
+        form = SampleForm(
+            data={
+                "name": "sample name",
+                "description": "sample description",
+                "sample_type": Sample.SampleType.DRUM,
+                "demo": "",
+                "tags": "tag1",
+            }
+        )
 
-        self.assertEquals(pre_request_samples_quantity, post_request_samples_quantity)
+        request = RequestFactory().post(_UPLOAD_URL, data=form.data)
+        request.user = self.user
+        request.FILES["sample_file"] = SimpleUploadedFile(
+            name="test", content=MOCK_SAMPLE_FILE
+        )
 
-    @patch("sample.views.SampleForm")
-    def test_post_request_valid_data(self, mock_SampleForm: MagicMock):
-        client = Client()
-        client.force_login(self.user)
+        response = upload(request)
 
-        mock_sample = Mock()
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, reverse("app:index"))
 
-        mock_sample_form = MockSampleForm(mock_sample)
-        mock_SampleForm.return_value = mock_sample_form
-
-        response = client.post(_UPLOAD_URL, follow=True)
-
-        self.assertRedirects(response, reverse("app:index"))
-
-        self.assertEquals(mock_sample_form.save.call_count, 1)
-        self.assertEquals(mock_sample.author, self.user)
+        if (sample := Sample.objects.first()) is None:
+            self.fail("Sample not created.")
+        else:
+            self.assertEqual(sample.name, "sample name")
+            self.assertEqual(sample.description, "sample description")
+            self.assertEqual(sample.sample_type, Sample.SampleType.DRUM)
 
     def test_tags_are_created(self) -> None:
         form = SampleForm(
@@ -112,17 +110,6 @@ class UploadViewsTestCase(TestCase):
         if sample is None:
             self.fail("Sample was not created.")
 
-        self.assertIsNotNone(sample.tags.filter(name="tag1").first())
-        self.assertIsNotNone(sample.tags.filter(name="tag2").first())
-        self.assertIsNotNone(sample.tags.filter(name="tag3").first())
-
-
-class MockSampleForm:
-    def __init__(self, mock_sample: Mock):
-        self.save = Mock(return_value=mock_sample)
-        self.save_m2m = Mock()
-
-        self.cleaned_data = {"tags": ""}
-
-    def is_valid(self):
-        return True
+        self.assertTrue(sample.tags.filter(name="tag1").exists())
+        self.assertTrue(sample.tags.filter(name="tag2").exists())
+        self.assertTrue(sample.tags.filter(name="tag3").exists())
