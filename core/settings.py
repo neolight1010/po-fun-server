d87aeae476df9bf50b9619ecc147bc7fa5fe4c1e
Anthony Suárez <neolight1010@gmail.com>
2023-04-30 15:52:56 -0500

    Use environment variables in settings

--- core/settings.py
+++ core/settings.py
@@ -10,24 +10,21 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import environ
 from pathlib import Path
 from typing import List
 import django_stubs_ext
 
 django_stubs_ext.monkeypatch()
 
+env = environ.Env()
+environ.Env.read_env(".env")
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-0gw%93tacl!pl=n@(dej@gk=!2yv-ifelca4i9yte-m8-z&*0$"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+SECRET_KEY = env.str("SECRET_KEY")
+DEBUG = env.bool("DEBUG")
 
 ALLOWED_HOSTS: List[str] = []
 
@@ -78,20 +75,9 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "core.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": env.db("DATABASE_URL"),
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -113,7 +99,6 @@ AUTH_USER_MODEL = "user.User"
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/3.2/topics/i18n/
 
 LANGUAGE_CODE = "en-us"
 
@@ -130,15 +115,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = env.str("STATIC_URL", default="static/")
 
-# Default primary key field type
-# https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# Auth URLs
+
 LOGIN_URL = "/login/"
 
 # Media
 MEDIA_URL = "/media/"
-MEDIA_ROOT = ""
+MEDIA_ROOT = env.str("MEDIA_ROOT", default="media/")
